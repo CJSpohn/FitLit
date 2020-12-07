5f733be5d59cba87568d6731d4fe28274fac0e8f
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-07 10:32:18 -0700

    Test all current data in console.

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,14 @@
-
-
 console.log("Hello World");
 
 let userId = 30;
 
 let allUsers = new UserRepository(userData);
 
+console.log(allUsers.getAvgStepGoal())
+
 let curUser = allUsers.getUserData(30)
 
 let curUserObject = new User(curUser);
 
 console.log(curUserObject)
+console.log(curUserObject.getUserFirstName())
