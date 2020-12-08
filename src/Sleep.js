98748d426ce62056039672e339a374b2eb2863d3
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-08 13:25:22 -0700

    Add getLifetimeSleepAvg

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,7 +1,21 @@
 class Sleep {
   constructor(user) {
-    this.userID = user.id
+    this.userID = user.id,
+    this.userSleepData = sleepData.filter(data => this.userID === data.userID);
   }
+
+  getLifetimeSleepAvg() {
+    let userSleepTotal = this.userSleepData.reduce((total, day) => {
+      total += day.hoursSlept;
+      return total;
+    }, 0)
+
+    let userSleepAvg = userSleepTotal / this.userSleepData.length;
+
+    return userSleepAvg;
+  }
+
+  
 
 
 }
