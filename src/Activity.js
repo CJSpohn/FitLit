1c9f6aee71fee796496e33517e9eb384fc596927
Luke Mason <lukjmason@gmail.com>
2020-12-08 13:55:00 -0700

    Create method in Activity class that gets the minutes active for a date range

--- src/Activity.js
+++ src/Activity.js
@@ -7,9 +7,27 @@ class Activity {
 
   getMilesForSpecificDate(date) {
     let numOfStepsOnDate = this.userActivityData.find(data => data.date === date).numSteps;
+
     const numOfMiles = (numOfStepsOnDate * this.strideLength) / 5280;
+
     return parseFloat(numOfMiles.toFixed(2));
   }
+
+  getMinutesActiveForSpecificDate(date) {
+    return this.userActivityData.find(data => data.date === date).minutesActive;
+  }
+
+  getMinutesActiveDataForRange(startDate, endDate) {
+    let firstIndex = this.userActivityData.findIndex(data => startDate === data.date);
+    let endIndex = this.userActivityData.findIndex(data => endDate === data.date);
+
+    let dataRange = this.userActivityData.slice(firstIndex, endIndex + 1);
+
+    let minutesActiverPerDay = dataRange.map(data => data.minutesActive);
+
+    return minutesActiverPerDay;
+  }
+
 
 
 }
