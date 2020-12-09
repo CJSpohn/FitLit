5325cef5bb55db61ebdbff5ca60a2050a79a1ac8
Luke Mason <lukjmason@gmail.com>
2020-12-09 15:23:41 -0700

    Link style sheet to html, start main page

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,14 @@
 const dropDownForUsers = document.querySelector('.js-all-users');
+const loginPage = document.querySelector('.js-login');
 
 let allUsers = new UserRepository(userData);
 
 const populateSelectUsers = () => allUsers.users.forEach(user => {
   dropDownForUsers.innerHTML += `
-    <option value='${user.id}'>${user.name}</option
+    <option value='${user.id}'>${user.name}</option>
     `;
 })
+
 
 
 
