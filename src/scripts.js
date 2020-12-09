b41d3943c20053ccffe3acebb1489a8fd44dcd0a
Luke Mason <lukjmason@gmail.com>
2020-12-09 15:54:11 -0700

    Make button functional on login page

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,29 @@
-const dropDownForUsers = document.querySelector('.js-all-users');
-const loginPage = document.querySelector('.js-login');
-
+//Global variables
 let allUsers = new UserRepository(userData);
 
-const populateSelectUsers = () => allUsers.users.forEach(user => {
+//query selectors
+const dropDownForUsers = document.querySelector('.js-all-users');
+const loginPage = document.querySelector('.js-login');
+const goToDashboardButton = document.querySelector('.js-enter-dashboard');
+const mainPage = document.querySelector('.js-main-page');
+
+//Event listeners
+window.onload = () => {
+  allUsers.users.forEach(user => {
   dropDownForUsers.innerHTML += `
     <option value='${user.id}'>${user.name}</option>
     `;
-})
+})};
 
+goToDashboardButton.addEventListener('click', goToDashboard);
 
+//Functions
+// const goToDashboard = () => {
+//   loginPage.classList.add('hidden');
+//   mainPage.classList.remove('hidden');
+// }
 
-
-populateSelectUsers();
+function goToDashboard() {
+  loginPage.classList.add('hidden');
+  mainPage.classList.remove('hidden');
+}
