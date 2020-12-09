ed233ef1212cb09fe65163d35e0943b908a8b792
Luke Mason <lukjmason@gmail.com>
2020-12-09 16:58:41 -0700

    Create info card for current user, and display average step goal

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,8 @@ const loginPage = document.querySelector('.js-login');
 const goToDashboardButton = document.querySelector('.js-enter-dashboard');
 const mainPage = document.querySelector('.js-main-page');
 const welcomeMessage = document.querySelector('.js-welcome');
+const infoCard = document.querySelector('.js-user-info');
+const allUserStepGoalCard = document.querySelector('.js-step-goal');
 
 //Functions
 const switchPage = () => {
@@ -24,10 +26,33 @@ const welcomeUser = () => {
   welcomeMessage.innerText = `Welcome ${currentUser.getUserFirstName()}`;
 }
 
+const compareStepGoals = () => {
+  const allUsersStepGoal = allUsers.getAvgStepGoal();
+  if (currentUser.dailyStepGoal > allUsersStepGoal) {
+    allUserStepGoalCard.innerHTML += `
+      <p>Your step goal of ${currentUser.dailyStepGoal} steps is ${currentUser.dailyStepGoal - allUsersStepGoal} steps more than the average user step goal!</p>
+    `
+  } else {
+    allUserStepGoalCard.innerHTML += `
+    <p>Your step goal of ${currentUser.dailyStepGoal} steps is ${allUsersStepGoal - currentUser.dailyStepGoal} steps fewer than the average user step goal!<p>
+    `
+  }
+}
+//REFACTOR
+const createUserInfo = () => {
+  for (let key in currentUser) {
+    infoCard.innerHTML += `
+      <p>${key}: ${currentUser[key]}</p>
+    `
+  }
+}
+
 const goToDashboard = () => {
   switchPage();
   instantiateUser();
   welcomeUser();
+  createUserInfo();
+  compareStepGoals();
 }
 
 
