96f0e3ff2017f3b7bff20f808392d4b7199899d1
Luke Mason <lukjmason@gmail.com>
2020-12-09 16:26:21 -0700

    Welcome user when you go to dashboard

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,36 @@
 //Global variables
 let allUsers = new UserRepository(userData);
+let currentUser;
 
 //query selectors
 const dropDownForUsers = document.querySelector('.js-all-users');
 const loginPage = document.querySelector('.js-login');
 const goToDashboardButton = document.querySelector('.js-enter-dashboard');
 const mainPage = document.querySelector('.js-main-page');
+const welcomeMessage = document.querySelector('.js-welcome');
 
 //Functions
-const goToDashboard = () => {
-  loginPage.classList.add('hidden');
-  mainPage.classList.remove('hidden');
-
-  const selectedUser = allUsers.getUserData(parseInt(dropDownForUsers.value));
-  const currentUser = new User(selectedUser);
-  console.log(currentUser);
-
-
+const switchPage = () => {
+  loginPage.classList.toggle('hidden');
+  mainPage.classList.toggle('hidden');
 }
+
+const instantiateUser = () => {
+  const selectedUser = allUsers.getUserData(parseInt(dropDownForUsers.value));
+  currentUser = new User(selectedUser);
+}
+
+const welcomeUser = () => {
+  welcomeMessage.innerText = `Welcome ${currentUser.getUserFirstName()}`;
+}
+
+const goToDashboard = () => {
+  switchPage();
+  instantiateUser();
+  welcomeUser();
+}
+
+
 
 
 
