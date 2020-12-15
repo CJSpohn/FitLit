5e8e3b4509cfef4a03ef54a2c4040947dcabf6c8
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-15 09:22:16 -0700

    Add tests for userRepository class

--- tests/userRepository-test.js
+++ tests/userRepository-test.js
@@ -5,14 +5,35 @@ const UserRepository = require('../src/UserRepository');
 const sampleData = require('../data/test-data');
 const sampleUserData = sampleData.sampleUserData;
 
-describe('UserRepository', function() {
+describe('UserRepository', () => {
+  let users;
 
   beforeEach(function() {
-    const users = new UserRepository(sampleUserData);
+    users = new UserRepository(sampleUserData);
   })
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   })
+
+  it('should contain a list of users', () => {
+    expect(users.users.length).to.equal(2);
+    expect(users.users).to.eql(sampleUserData)
+  })
+
+  it('should be able to return a specific user', () => {
+    let user1 = users.getUserData(1)
+    let user2 = users.getUserData(2)
+
+    expect(user1).to.eql(sampleUserData[0])
+    expect(user2).to.eql(sampleUserData[1])
+  })
+
+  it('should be able to return an average step goal for all users', () => {
+    let avgStepGoal = users.getAvgStepGoal()
+
+    expect(avgStepGoal).to.equal(7500)
+  })
+
 
 })
